9ab886443d522e6c37b8930b30485be122b8f295
Ilir Kosumi <ikosumi@lixar.com>
2016-03-29 19:59:37 +0200

    Created concrete implementation for Formatter interfase called PrintFormatter.java

---
+++ src/main/java/com/ikosumi/designpatterns/bridge/PrintFormatter.java
@@ -0,0 +1,19 @@
+package com.ikosumi.designpatterns.bridge;
+
+import java.util.List;
+
+public class PrintFormatter implements Formatter {
+    @Override
+    public String format(String header, List<Detail> details) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(header);
+        sb.append("\n");
+        for (Detail detail : details) {
+            sb.append(detail.getLabel());
+            sb.append(":");
+            sb.append(detail.getValue());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
